caaac5a10a481165df7069db77fd1f375a0152cb
Mateo Cabrera <mateo.cabrera@epitech.eu>
2025-01-17 00:20:49 +0100

    fix: add vercel domain to image domains whitelist

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
-        domains: ["*", "**"],
+        domains: [
+            "https://orange-business-website-refresh-concept-m53f-jsac5yeyo.vercel.app",
+            "*",
+            "**",
+        ],
         remotePatterns: [
             {
                 protocol: "https",
